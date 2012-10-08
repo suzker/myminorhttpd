f5a6a79042ae286f923335c50312c2d8317c360f
suzker <zsu@buffalo.edu>
2012-10-07 23:53:01 -0400

    modify queue implementation to a non-dynamic one

--- queue.c
+++ queue.c
@@ -1,24 +1,57 @@
 #include "queue.h"
 
-int enqueue(void * new_data){
-    if (_queue_num_element_ == _queue_num_allocated_){
-        _queue_num_allocated_ = _queue_num_allocated_ == 0 ? _queue_init_allocate_num_ : _queue_num_allocated_*2;
-        void ** _tmp = realloc(_queue_array_, (_queue_num_allocated_*sizeof(void *)));
-        if (!_tmp){
-            printf("[ERR] Couldn't re-allocate memory for the queue, exiting... \n");
-            return (-1);
-        }
-        _queue_array_ = _tmp;
-    }
-    _queue_array_[_queue_num_element_++]  = new_data;
+void ** _queue_array_;
+int _queue_idx_head_ = -1; // the first element of the queue
+int _queue_idx_last_ = -1; // the last element of the queue
+int _queue_num_element_ = 0;
+int _queue_size_ = 0;
+
+int queue_init(int size){
+    _queue_size_ = size;
+    _queue_array_ = malloc(_queue_num_element_*sizeof(void*));
+    if (!_queue_array_){return 1;}
+    _queue_idx_head_ = -1;
+    _queue_idx_last_ = -1;
+    _queue_num_element_ = 0;
     return 0;
 }
 
-void * dequeue(){
+int queue_enqueue(void * new_data){
+    if (_queue_idx_head_ == -1 && _queue_idx_last_ == -1){ 
+        _queue_array_[_queue_num_element_] = new_data;
+        _queue_idx_head_ = 0;
+        ++_queue_num_element_;
+        _queue_idx_last_ = _queue_idx_head_;
+    } else {
+        if (_queue_num_element_ < _queue_size_){
+            if (_queue_idx_last_ > _queue_size_ - 1){ // round up
+                _queue_idx_last_ = 0;
+            } else {
+                ++_queue_idx_last_;
+            }
+            _queue_array_[_queue_idx_last_] = new_data; 
+            ++_queue_num_element_;
+        } else {
+            return -1; //queue full
+        }
+    }
+    return 0;
+}
+
+void * queue_dequeue(){
     if (_queue_num_element_ > 0){
-        void * head_data = _queue_array_[0];
+        void * head_data = _queue_array_[_queue_idx_head_];
         // increase the pointer to the next queue element;
-        _queue_array_ = --_queue_num_allocated_ > 0 ? _queue_array_ = &(_queue_array_[1]) : NULL;
+        if (_queue_idx_head_ == _queue_idx_last_){ // if the last element popped out, start over
+            _queue_idx_head_ = -1;
+            _queue_idx_last_ = -1;
+        } else {
+            if (_queue_idx_head_ + 1 > _queue_size_ - 1){ // roundup
+                _queue_idx_head_ = 0;
+            } else {
+                ++_queue_idx_head_;
+            }
+        }
         --_queue_num_element_;
         return head_data;
     }
@@ -27,7 +60,9 @@ void * dequeue(){
 
 void queue_free(){
     free(_queue_array_);
-    _queue_array_ = NULL;
-    _queue_num_allocated_ = 0;
     _queue_num_element_ = 0;
+}
+
+int queue_get_num_elements(){
+    return _queue_num_element_;
 }
--- queue.h
+++ queue.h
@@ -3,34 +3,49 @@
     author: Zhiliang Su (zsu2[at]buffalo[dot]edu)
     revision: 07 Oct 2012
 */
+#ifndef _HEADER_QUEUE_
+#define _HEADER_QUEUE_
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void ** _queue_array_;
-int _queue_num_element_ = 0;
-int _queue_num_allocated_ = 0;
-const int _queue_init_allocate_num_ = 4;
+/**
+ function: queue_init
+  method to initialize a queue by passing a desired size for the queue
+  input:
+    int size: size
+  output:
+    int: 0 for normal otherwise abnormal
+*/
+int queue_init(int);
 
 /**
- function: enqueue
+ function: queue_enqueue
   method to add an element into the queue
   input:
     void *: a void pointer pointing to the to-be-added object
   output:
     int : 0 indicates success, otherwise unsuccess
 */
-int enqueue(void * new_data);
+int queue_enqueue(void * new_data);
 
 /**
- function: dequeue
+ function: queue_dequeue
   method to pop the head data off the queue
   output:
     void * : pointer to that specific head data. NULL if unsuccess
 */
-void * dequeue();
+void * queue_dequeue();
 
 /**
  function: queue_free
   to free the memories used by the queue
 */
 void queue_free();
+
+/**
+ function: queue_get_num_elements
+  to get the number of elements
+*/
+int queue_get_num_elements();
+#endif
